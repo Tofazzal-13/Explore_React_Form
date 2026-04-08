4ac7374e771ccdc9299bb516d335f65e1b10a194
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-04-08 10:15:26 +0600

    Access form data using onSubmit and e target

---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder='Your name' />
+                <br />
+                <input type="email" name='email' placeholder='Your email' />
+                <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
